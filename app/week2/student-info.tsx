112a2e298bce84ba7838b382084c21a5479a4041
ashhadq83 <ashhadq83@gmail.com>
2026-01-20 15:19:47 -0700

    Update student-info.tsx

--- app/week2/student-info.tsx
+++ app/week2/student-info.tsx
@@ -4,17 +4,20 @@ import Link from 'next/link';
 type Student = {
   name: string;
   github: string;
+  id: string
 };
 
 const StudentInfo: React.FC = () => {
   const student: Student = {
-    name: 'Ashhad',
+    name: 'Ashhad Shakeel',
+    id: '000956497',
     github: 'https://github.com/ashhadq83/cprg_assignments',
   };
 
   return (
     <div style={{ padding: '2rem', fontFamily: 'Inter, sans-serif' }}>
       <h1>{student.name}</h1>
+      <h1>{student.id}</h1>
       <p>
         GitHub:{" "}
         <Link href={student.github}>
